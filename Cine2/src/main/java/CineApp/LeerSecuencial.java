aca8f4a791c218de885219df4cb9c0bb028ac1db
Alex Pineño Sanchez <machupichu0409@gmail.com>
2023-10-03 20:28:20 +0200

    Funciones LeerSecuencial

--- Cine2/src/main/java/CineApp/LeerSecuencial.java
+++ Cine2/src/main/java/CineApp/LeerSecuencial.java
@@ -1,5 +1,97 @@
 package CineApp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class LeerSecuencial {
+
+    /**
+     *
+     * @author Alex Pineño Sanchez
+     *
+     * @param nombreArchivo
+     */
+    String nombreArchivo;
+    FileReader archivoLectura;
+
+    /**
+     *
+     * @author Alex Pineño Sanchez
+     * 
+     * @param nombreArchivo
+     * @param archivoLectura
+     */
+    public LeerSecuencial(String nombreArchivo, FileReader archivoLectura) {
+        this.nombreArchivo = nombreArchivo;
+        this.archivoLectura = archivoLectura;
+    }
+
+    /**
+     *@author Alex Pineño Sanchez
+     */
+    public void leerSecuencial() {
+        try {
+            this.archivoLectura = new FileReader(nombreArchivo);
+            BufferedReader bufferLectura = new BufferedReader(archivoLectura);
+
+            String linea;
+            while ((linea = bufferLectura.readLine()) != null) {
+                System.out.println(linea);
+            }
+
+            bufferLectura.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *@author Alex Pineño Sanchez
+     * 
+     * @param nombreArchivo
+     */
+    public static void leerSecuencial(String nombreArchivo) {
+        try {
+            FileReader archivoLectura = new FileReader(nombreArchivo);
+            BufferedReader bufferLectura = new BufferedReader(archivoLectura);
+
+            String linea;
+            while ((linea = bufferLectura.readLine()) != null) {
+                System.out.println(linea);
+            }
+
+            bufferLectura.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     *
+     * @author Alex Pineño Sanchez
+     * 
+     * @param nombreArchivo
+     * @param archivoLectura
+     */
+    public static void leerSecuencial(String nombreArchivo, FileReader archivoLectura) {
+        try {
+            archivoLectura = new FileReader(nombreArchivo);
+            BufferedReader bufferLectura = new BufferedReader(archivoLectura);
+
+            String linea;
+            while ((linea = bufferLectura.readLine()) != null) {
+                System.out.println(linea);
+            }
+
+            bufferLectura.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
