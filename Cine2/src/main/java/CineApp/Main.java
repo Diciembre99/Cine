6c3b2e1375c6d6c7408e23e698e4d0053b327ec8
Kevin Castillo <krenecas99@gmail.com>
2023-10-10 20:52:21 +0200

    Revert "Funciones Secuencial :)"
    This reverts commit 06207d24

--- Cine2/src/main/java/CineApp/Main.java
+++ Cine2/src/main/java/CineApp/Main.java
@@ -2,21 +2,26 @@ package CineApp;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         
-        Date begin = null;
-        Date end = null;
+        LocalDate begin = null;
+        LocalDate end = null;
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        //EscribirSecuencial.escribirSecuencial(pelicula);
-        //EscribirSecuencial.escribirSecuencial(pelicula2);
+       Billboard pelicula=new Billboard("batman",begin,end,Gender.ACTION,AgeCategory.C_18);
+       Billboard pelicula2=new Billboard("barbie",begin,end,Gender.ADVENTURE,AgeCategory.C_A);
+        
+        EscribirSecuencial.escribirSecuencialBillboard(pelicula);
+        EscribirSecuencial.escribirSecuencialBillboard(pelicula2);
         
         System.out.println("Funcion leer:");
         LeerSecuencial.leerSecuencial();
+        
         
         
         
